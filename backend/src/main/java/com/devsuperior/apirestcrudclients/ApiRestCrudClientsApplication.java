530d855ffea08ca14ffd93a636f007832687e56d
arthurtavora <arthurtavorademello@gmail.com>
2021-08-27 17:03:04 -0300

    Create backend project

---
+++ backend/src/main/java/com/devsuperior/apirestcrudclients/ApiRestCrudClientsApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.apirestcrudclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCrudClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCrudClientsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/apirestcrudclients/ApiRestCrudClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.apirestcrudclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCrudClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
